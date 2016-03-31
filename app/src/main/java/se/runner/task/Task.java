7f449d9b4139d61a0a28b2f73eef74423dd3dbcd
Zieng <lijiebug@gmail.com>
2016-04-01 07:48:45 +0800

    save changes

--- app/src/main/java/se/runner/task/Task.java
+++ app/src/main/java/se/runner/task/Task.java
@@ -8,21 +8,22 @@ public class Task
     {
         INIT,   // when created, it is in INIT status. laucher can discard task in this state.
         RELEASED,  // when laucher release it to internet(so other can see it), it's in RELEASED.  laucher can discard task in this state.
-        PROGRESS,  // when taker(or runner) takes the task, it's in progress.  laucher/runner can discard task in this state only when other agree.
+        PROGRESS,  // when taker(or runner) takes the task, it's in progress.or task resumed from paused.  laucher/runner can discard task in this state only when other agree.
         ABORT,   // launcher/runner discard task and other agreed.
         PAUSED,  // task is paused, so delay time can be infty, but the task can resume later.
-        RESUMED,  // task resumed from paused
         COMPLETED,  // when task is done in a normal way
         RATED,   // after the task is rated
-    };
+    }
 
     private int id;
     private int type;
     private double rate;  // [0~5]
     private TaskStatus status;
 
+    // use System.currentTimeMillis() may not be safe, but simple
     private long create_timestamp;
     private long release_timestamp;
+    private long deadline_timestamp;
     private long finish_timestamp;  // finish includes abort or completed
 
     private String comment;
@@ -53,9 +54,34 @@ public class Task
 
     }
 
-    public void done()
+    public void releaseTask()
     {
+        setStatus(TaskStatus.RELEASED);
+        // TODO: 4/1/16 release task
+    }
 
+    public void resumeTask()
+    {
+        setStatus(TaskStatus.PROGRESS);
+        // TODO: 4/1/16 resumeTask
+    }
+
+    public void pauseTask()
+    {
+        setStatus(TaskStatus.PAUSED);    
+        //// TODO: 4/1/16  pause task
+        deadline_timestamp = -1;
+    }
+    
+    public void abortTask()
+    {
+        setStatus(TaskStatus.ABORT);
+        //// TODO: 4/1/16 abort task
+    }
+    public void comleteTask()
+    {
+        setStatus(TaskStatus.COMPLETED);
+        //// TODO: 4/1/16 complete task
     }
 
     private void setStatus(TaskStatus t)
@@ -80,9 +106,10 @@ public class Task
 
     public boolean setRate(double rate)
     {
-        if( rate <0 || rate > 5)
+        if( rate <0 || rate > 5 || status!= TaskStatus.COMPLETED )
             return false;
 
+        setStatus(TaskStatus.RATED);
         this.rate = rate;
         return true;
     }
@@ -97,8 +124,8 @@ public class Task
 
     }
 
-    public String getComment(String comment)
+    public String getComment()
     {
-        return null;
+        return comment;
     }
 }
