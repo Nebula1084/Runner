f227571cae19e44b649dbda53a4535444a24eb78
Zieng <lijiebug@gmail.com>
2016-04-10 20:58:29 +0800

    prepare for map

--- app/src/main/java/se/runner/ui/MapActivity.java
+++ app/src/main/java/se/runner/ui/MapActivity.java
@@ -1,19 +1,47 @@
 package se.runner.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.LocationSource;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.model.MyLocationStyle;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import se.runner.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements LocationSource,AMapLocationListener{
     final public static String LONGITUDE = "longitude";
     final public static String LATITUDE = "latitude";
+
+    private AMap aMap;
+    private MapView mapView;
+    private LocationSource.OnLocationChangedListener mListener;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+
 
     private Intent result;
     @Bind(R.id.tool_bar)
@@ -29,12 +57,192 @@ public class MapActivity extends AppCompatActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        AMapLocationClient.setApiKey("6fb01cc4afb8c3c461b106a89d16d558");
+
+
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        init();
+
+
         result = new Intent();
         /*This part is demo*/
         result.putExtra(LONGITUDE, 1.1);
         result.putExtra(LATITUDE, 1.1);
         /*Until here*/
+
+        Log.e("Map","sha1="+sHA1(this));
     }
+
+    private void init()
+    {
+        if( aMap == null)
+        {
+            aMap = mapView.getMap();
+            setUpMap();
+        }
+    }
+
+    private void setUpMap()
+    {
+        // 自定义系统定位小蓝点
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory
+                .fromResource(R.drawable.location_marker));// 设置小蓝点的图标
+        myLocationStyle.strokeColor(Color.BLACK);// 设置圆形的边框颜色
+        myLocationStyle.radiusFillColor(Color.argb(100, 0, 0, 180));// 设置圆形的填充颜色
+        // myLocationStyle.anchor(int,int)//设置小蓝点的锚点
+        myLocationStyle.strokeWidth(1.0f);// 设置圆形的边框粗细
+
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // aMap.setMyLocationType()
+
+        // set marker
+        addMarkersToMap();
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation)
+    {
+        if (mListener != null && amapLocation != null)
+        {
+//            Log.e("onLocationChanged","mListener and amapLocation are not null");
+
+            if (amapLocation != null && amapLocation.getErrorCode() == 0)
+            {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+            }
+            else
+            {
+                String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
+                Log.e("AmapErr", errText);
+            }
+        }
+    }
+
+    /*
+    * 开始定位
+    * */
+
+    @Override
+    public void activate(OnLocationChangedListener listener)
+    {
+        mListener = listener;
+        if (mlocationClient == null)
+        {
+//            Log.e("activate","mlocationClient is null");
+            if(this == null)
+                Log.e("activate","this context is null");
+
+            mlocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mlocationClient.startLocation();
+        }
+        else
+            Log.e("activate","mlocationClient is not null");
+    }
+
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate()
+    {
+        mListener = null;
+        if (mlocationClient != null)
+        {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
+    private void addMarkersToMap()
+    {
+        aMap.addMarker(new MarkerOptions().anchor(0.5f, 0.5f)
+                .position(new LatLng(31.238068, 121.501654)).title("您的配送员")
+                .snippet("Test").draggable(false));
+    }
+
+
+    // for debug
+    public static String sHA1(Context context)
+    {
+        try
+        {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), PackageManager.GET_SIGNATURES);
+
+            byte[] cert = info.signatures[0].toByteArray();
+
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(cert);
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < publicKey.length; i++)
+            {
+                String appendString = Integer.toHexString(0xFF & publicKey[i]).toUpperCase(Locale.US);
+                if (appendString.length() == 1)
+                    hexString.append("0");
+                hexString.append(appendString);
+                hexString.append(":");
+            }
+            return hexString.toString();
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
